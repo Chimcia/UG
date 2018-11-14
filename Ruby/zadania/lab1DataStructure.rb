5121144cd75500e77eaec72408fe70c29c51cf48
Ewelina <38459295+Chimcia@users.noreply.github.com>
2018-11-14 12:08:05 +0100

    Add files via upload

---
+++ Ruby/zadania/lab1DataStructure.rb
@@ -0,0 +1,34 @@
+# EASY
+
+# Write a method that returns a boolean indicating whether an array is in sorted order. Use the equality operator (==), which returns a boolean indicating whether its operands are equal, e.g., 2 == 2 => true, ["cat", "dog"] == ["dog", "cat"] => false
+def in_order?(arr)
+  # your code goes here
+end
+
+
+# MEDIUM
+
+# Write a method that returns the range of its argument (an array of integers).
+def range(arr)
+  # your code goes here
+end
+
+
+# HARD 
+
+# Write a method that returns an array of the digits of a non-negative integer in descending order and as strings, e.g., descending_digits(4291) #=> ["9", "4", "2", "1"]
+def descending_digits(int)
+  # your code goes here
+end
+
+# Write a method that converts an array of ten integers into a phone number in the format "(123) 456-7890".
+def to_phone_number(arr)
+  # your code goes here
+end
+
+# Write a method that returns the range of a string of comma-separated integers, e.g., str_range("4,1,8") #=> 7
+def str_range(str)
+  # your code goes here
+end
+
+require_relative "test.rb"
