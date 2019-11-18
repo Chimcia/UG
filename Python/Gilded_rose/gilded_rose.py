12c2e5db92c0bb903eace4a4632aa30b1815abc3
Ewelina <38459295+Chimcia@users.noreply.github.com>
2019-11-18 13:43:18 +0100

    Update gilded_rose.py

--- Python/Gilded_rose/gilded_rose.py
+++ Python/Gilded_rose/gilded_rose.py
@@ -13,36 +13,40 @@ class GildedRose(object):
                 continue
 
             if item.name == "Aged Brie":
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+                self.increase_quality(item)   #Przydatne - Refactor->extract->method
                 item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self.increase_quality(item)
                 continue
 
-            if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 6:
+                    item.quality = item.quality +3
+                elif item.sell_in < 11:
+                    item.quality = item.quality +2
                 else:
-                    item.quality = item.quality - item.quality
+                    item.quality = item.quality +1
 
+                item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
+                    item.quality = 0
+                item.quality = min(item.quality, 50)
+                continue
+
+            else:
+                item.quality = item.quality -1
+                item.quality = max(item.quality, 0)  # Nigdy nie zejdzie pon 0
+
+                item.sell_in= item.sell_in -1
+
+                if item.sell_in < 0:
+                    item.quality = item.quality - 1
+                    item.quality = max(item.quality, 0)
+                   
+    def increase_quality(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+                
 
 class Item:
     def __init__(self, name, sell_in, quality):
